7bd051802634079a58b62455c30785e08ac49ad4
Helene Normant <normanthelene@gmail.com>
2022-06-26 14:38:58 +0200

    fix email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,28 +10,30 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs.sendForm('service_jtefeih', 'template_cp7n91k', form.current, 'ut-QCXmhwtfNQ95Iu')
+    alert('Message envoyé');
     e.target.reset()
-
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
-  };
+  }
+
 
   return (
     <section id="contact">
       <h5>ME CONTACTER</h5>
-      <div className='container contact__container'> 
+      <div className='container contact__container'>
         <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Votre nom' required />
-          <input type="email" name="email" placeholder='votre email' required />
+          <input type="email" name="email" placeholder='Votre email' required />
           <textarea name="message" rows="7" placeholder='Votre message' required></textarea>
           <button type='submit' className='btn btn-primary'>Envoyer</button>
-        </form> 
+        </form>
       </div>
     </section>
-  )
+  );
 }
+
 
 export default Contact
